f9dd8f74b70f4ee8409808d7251ecf315ed622aa
basant <basantlazem4@outlook.com>
2023-04-23 12:18:13 -0700

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,3 @@
-#include "holberton.h"
-
 /**
  * _puts_recursion - a function that prints a new line
  *
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,3 @@
-#include "holberton.h"
-
 /**
  * _print_rev_recursion - prints a string in reverse
  *
